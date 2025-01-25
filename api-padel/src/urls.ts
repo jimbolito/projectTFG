f931aad8223a5cfd891486bd3e9b1c1c773846fe
Jimmy Coque Yanez <jimmycoqueyanez1991@gmail.com>
2025-01-26 00:00:10 +0100

    added new urls padel

--- api-padel/src/urls.ts
+++ api-padel/src/urls.ts
@@ -1,18 +1,45 @@
 const urls = {
-  shoesCategory: {
-    Adidas: 'https://padelmarket.com/es-eu/collections/zapatillas?filter.p.vendor=Adidas',
-    Babolat: 'https://padelmarket.com/es-eu/collections/zapatillas?filter.p.vendor=Babolat',
-    Bullpadel: 'https://padelmarket.com/es-eu/collections/zapatillas?filter.p.vendor=Bullpadel',
-  },
   padelCategory: {
     Adidas: 'https://padelmarket.com/es-eu/collections/palas?filter.p.vendor=Adidas',
     Babolat: 'https://padelmarket.com/es-eu/collections/palas?filter.p.vendor=Babolat',
     Bullpadel: 'https://padelmarket.com/es-eu/collections/palas?filter.p.vendor=Bullpadel',
+    BlackCrown: 'https://padelmarket.com/es-eu/collections/palas?filter.p.vendor=Black+Crown',
+    Dunlop: 'https://padelmarket.com/es-eu/collections/palas?filter.p.vendor=Dunlop',
+    Enebe: 'https://padelmarket.com/es-eu/collections/palas?filter.p.vendor=Enebe',
+    Puma: 'https://padelmarket.com/es-eu/collections/palas?filter.p.vendor=Puma',
+    Vibora: 'https://padelmarket.com/es-eu/collections/palas?filter.p.vendor=Vibora',
+    Wilson: 'https://padelmarket.com/es-eu/collections/palas?filter.p.vendor=Wilson'
+  },
+  shoesCategory: {
+    Adidas: 'https://padelmarket.com/es-eu/collections/zapatillas?filter.p.vendor=Adidas',
+    Babolat: 'https://padelmarket.com/es-eu/collections/zapatillas?filter.p.vendor=Babolat',
+    Bullpadel: 'https://padelmarket.com/es-eu/collections/zapatillas?filter.p.vendor=Bullpadel',
+    Joma: 'https://padelmarket.com/es-eu/collections/zapatillas?filter.p.vendor=Joma',
+    Puma: 'https://padelmarket.com/es-eu/collections/zapatillas?filter.p.vendor=Puma',
+    Wilson: 'https://padelmarket.com/es-eu/collections/zapatillas?filter.p.vendor=Wilson'
+  },
+  clothCategory: {
+    Adidas: 'https://padelmarket.com/es-eu/collections/ropa?filter.p.vendor=Adidas',
+    Babolat: 'https://padelmarket.com/es-eu/collections/ropa?filter.p.vendor=Babolat',
+    Bullpadel: 'https://padelmarket.com/es-eu/collections/ropa?filter.p.vendor=Bullpadel',
+    Nox: 'https://padelmarket.com/es-eu/collections/ropa?filter.p.vendor=Nox',
+    Puma: 'https://padelmarket.com/es-eu/collections/ropa?filter.p.vendor=Puma',
+
   },
   ballsCategory: {
     Adidas: 'https://padelmarket.com/es-eu/collections/pelotas-de-padel?filter.p.vendor=Adidas',
     Bullpadel: 'https://padelmarket.com/es-eu/collections/pelotas-de-padel?filter.p.vendor=Bullpadel',
+    Head: 'https://padelmarket.com/es-eu/collections/pelotas-de-padel?filter.p.vendor=Head',
+    Wilson: 'https://padelmarket.com/es-eu/collections/pelotas-de-padel?filter.p.vendor=Wilson',
+    StarVie: 'https://padelmarket.com/es-eu/collections/pelotas-de-padel?filter.p.vendor=StarVie'
   },
+  accesoriesCategory: {
+    Adidas: 'https://padelmarket.com/es-eu/collections/accesorios?filter.p.vendor=Adidas',
+    Babolat: 'https://padelmarket.com/es-eu/collections/accesorios?filter.p.vendor=Babolat',
+    Bullpadel: 'https://padelmarket.com/es-eu/collections/accesorios?filter.p.vendor=Bullpadel',
+    Wilson: 'https://padelmarket.com/es-eu/collections/accesorios?filter.p.vendor=Wilson',
+    Nox: 'https://padelmarket.com/es-eu/collections/accesorios?filter.p.vendor=Nox'
+  }
 };
 
 export default urls;
--- webpadelapp/src/app/components/category/category.component.ts
+++ webpadelapp/src/app/components/category/category.component.ts
@@ -19,6 +19,11 @@ export class CategoryComponent implements OnInit, OnDestroy {
 
   selectedProduct: any | null = null; // Producto seleccionado para el modal
   currentImageIndex: number = 0; // Índice actual de la imagen en el modal
+
+  // Variables para controlar el cambio de imagen en la tarjeta
+  hoveredProduct: any | null = null; // Producto actualmente bajo el ratón
+  hoveredImageIndex: number = 0; // Índice actual de la imagen al pasar el ratón
+
   private routeSubscription: Subscription | null = null;
 
   constructor(
@@ -80,5 +85,28 @@ export class CategoryComponent implements OnInit, OnDestroy {
         (this.currentImageIndex - 1 + this.selectedProduct.images.length) %
         this.selectedProduct.images.length;
     }
+  }
+
+  // Eventos de ratón
+  onMouseEnter(product: any): void {
+    this.hoveredProduct = product;
+    this.hoveredImageIndex = 1; // Cambia a la segunda imagen
+    if (product.images.length > 1) {
+      // Inicia el bucle de cambio de imágenes
+      let index = 1;
+      const interval = setInterval(() => {
+        if (this.hoveredProduct !== product) {
+          clearInterval(interval);
+        } else {
+          this.hoveredImageIndex = index % product.images.length;
+          index++;
+        }
+      }, 1000); // Cambia la imagen cada 1 segundo
+    }
+  }
+
+  onMouseLeave(): void {
+    this.hoveredProduct = null;
+    this.hoveredImageIndex = 0; // Regresa a la primera imagen
   }
 }
