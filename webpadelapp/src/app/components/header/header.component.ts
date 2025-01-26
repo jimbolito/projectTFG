16ae2e97898c94d13aca548ebdd88d5ff5364e48
Jimmy Coque Yanez <jimmycoqueyanez1991@gmail.com>
2025-01-26 17:36:25 +0100

    added subcategoris in all productos

--- webpadelapp/src/app/components/header/header.component.ts
+++ webpadelapp/src/app/components/header/header.component.ts
@@ -1,54 +1,3 @@
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { RouterModule } from '@angular/router';
-
-// @Component({
-//   selector: 'app-header',
-//   standalone: true,
-//   templateUrl: './header.component.html',
-//   styleUrls: ['./header.component.css'],
-//   imports: [CommonModule, RouterModule], // Importa los módulos necesarios
-// })
-// export class HeaderComponent {
-//   categories = [
-//     {
-//       name: 'Palas de padel',
-//       subcategories: ['Adidas', 'Bullpadel', 'Babolat', 'Puma'],
-//       open: false,
-//     },
-//     {
-//       name: 'Zapatillas',
-//       subcategories: ['Adidas', 'Bullpadel', 'Babolat', 'Puma'],
-//       open: false,
-//     },
-//     {
-//       name: 'Ropa',
-//       subcategories: ['Adidas', 'Bullpadel', 'Babolat', 'Puma'],
-//       open: false,
-//     },
-//     {
-//       name: 'Pelotas',
-//       subcategories: ['Adidas', 'Bullpadel', 'Babolat', 'Puma'],
-//       open: false,
-//     },
-//     {
-//       name: 'Accesorios',
-//       subcategories: ['Adidas', 'Bullpadel', 'Babolat', 'Puma'],
-//       open: false,
-//     },
-
-//   ];
-
-//   toggleSubmenu(categoryName: string): void {
-//     this.categories = this.categories.map((cat) =>
-//       cat.name === categoryName ? { ...cat, open: !cat.open } : { ...cat, open: false }
-//     );
-//   }
-// }
-
-
-//Despues del servicio:
-
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
@@ -64,27 +13,27 @@ export class HeaderComponent {
   categories = [
     {
       name: 'Palas de padel',
-      subcategories: ['Adidas', 'Bullpadel', 'Babolat', 'Puma'],
+      subcategories: ['Adidas', 'Babolat', 'Bullpadel', 'BlackCrown', ,'Dunlop', 'Enebe','Puma', 'Vibora', 'Wilson'],
       open: false,
     },
     {
       name: 'Zapatillas',
-      subcategories: ['Adidas', 'Bullpadel', 'Babolat', 'Puma'],
+      subcategories: ['Adidas',  'Babolat', 'Bullpadel', 'BlackCrown', ,'Dunlop', 'Enebe','Puma', 'Vibora', 'Wilson'],
       open: false,
     },
     {
       name: 'Ropa',
-      subcategories: ['Adidas', 'Bullpadel', 'Babolat', 'Puma'],
+      subcategories: ['Adidas',  'Babolat', 'Bullpadel', 'BlackCrown', ,'Dunlop', 'Enebe','Puma', 'Vibora', 'Wilson'],
       open: false,
     },
     {
       name: 'Pelotas',
-      subcategories: ['Adidas', 'Bullpadel', 'Babolat', 'Puma'],
+      subcategories: ['Adidas',  'Babolat', 'Bullpadel', 'BlackCrown', ,'Dunlop', 'Enebe','Puma', 'Vibora', 'Wilson'],
       open: false,
     },
     {
       name: 'Accesorios',
-      subcategories: ['Adidas', 'Bullpadel', 'Babolat', 'Puma'],
+      subcategories: ['Adidas',  'Babolat', 'Bullpadel', 'BlackCrown', ,'Dunlop', 'Enebe','Puma', 'Vibora', 'Wilson'],
       open: false,
     }
   ];
